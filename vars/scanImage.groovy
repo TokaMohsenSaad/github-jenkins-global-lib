ff2f5205a12c6fe68f65b506bdee5c7e827e7d2d
Toka Mohsen <tokasaad2012@gmail.com>
2026-06-03 02:19:45 +0300

    all libraries

--- vars/scanImage.groovy
+++ vars/scanImage.groovy
@@ -1,3 +1,3 @@
 def call(String imageName, String tag) {
-    sh "trivy image --exit-code 0 --severity HIGH, CRITICAL ${imageName}:${tag}"
+    sh "trivy image --exit-code 0 --severity HIGH,CRITICAL ${imageName}:${tag}"
 }
