3f1c7be18b958c901318ddf4f803b1a47228dec7
Toka Mohsen <tokasaad2012@gmail.com>
2026-06-03 11:25:50 +0300

    all libraries

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,5 +1,5 @@
 def call (String imageName, String tag, String registry) {
     sh """ echo "Pushing Image"
-    docker push ${registry}/${imageName}:${tag}"
+    docker push ${registry}/${imageName}:${tag}
 """
 }
