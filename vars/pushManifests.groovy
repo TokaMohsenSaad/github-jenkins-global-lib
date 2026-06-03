405e06b8a573761fe8e38f24d2cbbcb766b4108b
Toka Mohsen <tokasaad2012@gmail.com>
2026-06-03 11:55:34 +0300

    all libraries

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -3,7 +3,7 @@ def call(String git_credentials, String repoUrl) {
         sh """
             git config user.email "jenkins@clouddevops.com"
             git config user.name "Jenkins CI"
-            git add kubernetes/deployment.yaml
+            git add kubernetes/deployment.yml
             git commit -m "ci: update image tag via Jenkins" || true
             git push https://${GIT_USER}:${GIT_PASS}@${repoUrl} HEAD:master
         """
--- vars/updateManifests.groovy
+++ vars/updateManifests.groovy
@@ -1,4 +1,4 @@
-def call(String imageName, String tag, String registry, String manifestPath = "kubernetes/deployment.yaml"){
+def call(String imageName, String tag, String registry, String manifestPath = "kubernetes/deployment.yml"){
     sh """ echo "Updating Manifest"
      sed -i 's|image: .*|image: ${registry}/${imageName}:${tag}|g' ${manifestPath}
      """
